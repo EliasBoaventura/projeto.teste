897decfabe668de05557c662cde8ff5aef55a886
Elias boaventura <elias4144@hotmail.com>
2023-04-16 11:55:18 -0300

    projeto

---
+++ src/main/java/com/teste/Contato.java
@@ -0,0 +1,29 @@
+package com.teste;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Contato {
+
+	private String id;
+	private String nome;
+	private String telefone;
+
+	public Contato() {
+
+	}
+
+	public Contato(String id, String nome, String telefone) {
+
+		this.id = id;
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+
+	public boolean isNovo() {
+		return id == null;
+	}
+
+}
